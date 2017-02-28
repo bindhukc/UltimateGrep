638800dd5b9ba0fb88d466f48ec9265bac6c8465
codinggunda <bindhu12.prasanna@gmail.com>
2017-02-28 15:52:28 -0800

    added one more line

--- src/main/java/com/gubbacchi/tools/UltimateGrep.java
+++ src/main/java/com/gubbacchi/tools/UltimateGrep.java
@@ -8,5 +8,10 @@ public class UltimateGrep {
         System.out.println("Hello world");
         System.out.println("Hi Puttanni");
         System.out.println("Hi Good morning Gubbi mari");
+        System.out.println("I changed something");
+
+        // 1. argument parsing  "grep <-n> search_str filename"
+        // 2. argument validation
+        // 3. reading file line-by-line and and searching for string
     }
 }
