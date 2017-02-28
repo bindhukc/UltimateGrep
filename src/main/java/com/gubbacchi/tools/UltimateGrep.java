0c4964cea299b5e8291a0f87df33ca69acdf885b
Bindhu KC <bindhukc@Bindhus-MacBook-Pro.local>
2017-02-27 22:58:07 -0800

    program to print hello world

---
+++ src/main/java/com/gubbacchi/tools/UltimateGrep.java
@@ -0,0 +1,10 @@
+package com.gubbacchi.tools;
+
+/**
+ * Created by bindhukc on 2/27/17.
+ */
+public class UltimateGrep {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
