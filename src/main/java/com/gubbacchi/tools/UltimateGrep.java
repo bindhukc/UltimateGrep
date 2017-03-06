45b329a31bcb9e923d17721d1e93fbca748e7d42
codinggunda <bindhu12.prasanna@gmail.com>
2017-03-06 05:08:15 -0800

    new lines

--- src/main/java/com/gubbacchi/tools/UltimateGrep.java
+++ src/main/java/com/gubbacchi/tools/UltimateGrep.java
@@ -7,57 +7,61 @@ import java.util.Scanner;
  * Created by bindhukc on 2/27/17.
  */
 public class UltimateGrep {
-    public static void main(String[] args) {
-
-        System.out.println("Arg 1 : " + args[0]);
-
-        System.out.println("Hello world");
-        System.out.println("Hi Puttanni");
-        System.out.println("Hi Good morning Gubbi mari");
-        System.out.println("I changed something");
-        System.out.println("Helooo");
+    public static void main(String[] args) throws IOException {
+        //System.out.println("Arg 1 : " + args[0]);
+        //System.out.println("Arg 2 : " + args[1]);
         FileInputStream fis = null;
         BufferedReader b = null;
-        try {
-            fis = new FileInputStream("/Users/bindhukc/code/UltimateGrep/src/main/java/com/gubbacchi/tools/sample.txt");
-            b = new BufferedReader(new InputStreamReader(fis));
-            String line = b.readLine();
-            while (line != null) {
-                //System.out.println(line);
-                line = b.readLine();
+        System.out.println("enter the string you want to search in a file");
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        System.out.println("the input string is  "+ input);
+        int linenum = 0;
+        int flag = 0;
+        fis = new FileInputStream("/Users/bindhukc/code/UltimateGrep/src/main/java/com/gubbacchi/tools/sample.txt");
+        b = new BufferedReader(new InputStreamReader(fis));
+        String lineFromfile = b.readLine();
+        while(lineFromfile != null ) {
+            linenum = linenum + 1;
+            if(lineFromfile.contains(input) && lineFromfile.equalsIgnoreCase(input)){
+                System.out.println("i found string ::  "+input+"  present in the lines "+linenum);
+                flag = 1;
             }
-        }
-        catch (FileNotFoundException ex){
-            System.out.println("File not found");
-        }
-        catch (IOException ex1){
-            System.out.println("IO exception");
-        }
-        finally {
-                System.out.println("Everything is fine");
-            }
-           System.out.println("enter the string you want to search in a file");
-           Scanner s = new Scanner(System.in);
-           String input = s.nextLine();
-           System.out.println("the input string is  "+ input);
 
-               Scanner s1 = new Scanner("/Users/bindhukc/code/UltimateGrep/src/main/java/com/gubbacchi/tools/sample.txt");
-               while(s1.hasNextLine()) {
-                   String lineFromfile = s1.nextLine();
-                   if(lineFromfile.contains(input)){
-                       System.out.println("i found string in the file "+input);
-                   }
-               }
+            lineFromfile = b.readLine();
+
         }
-
-
-        // 1. argument parsing  "grep <-n> search_str filename"
-        // 2. argument validation
-        // 3. reading file line-by-line and and searching for string
+        if(flag == 0){
+            System.out.println("not found");
+        }
     }
 
 
-    // 1. remove hello world statements
-    // 2. use buffered reader instead of scanner to read file
-    // 3. print line numbers
-    // 4. Get search string from command line instead of scanner
+    // 1. argument parsing  "grep <-n> search_str filename"
+    // 2. argument validation
+
+    // 3. reading file line-by-line and and searching for string
+}
+
+
+// 1. remove hello world statements
+// 2. use buffered reader instead of scanner to read file
+// 3. print line numbers
+// 4. Get search string from command line instead of scanner
+// 5. implement your own contains
+// 6. refactor -- split the code into multiple functions and classes
+// 7. take filename from the argumentl'
+
+// week target:
+// maven
+// jar
+// jar vs class files
+// git commands
+// multiple classes -- object create, use etc.
+
+// week 2:
+// spill overs from last week
+// pom.xml -- using packages
+// command line argument package (library)
+//
+
